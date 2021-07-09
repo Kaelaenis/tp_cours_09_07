26959c16f88770135c6f32b1345353355626e34f
Maxime Lopez <maxime53@live.com>
2021-07-09 11:21:04 +0200

    123

--- liste_commandes.php
+++ liste_commandes.php
@@ -10,17 +10,25 @@
         <?php require("class.php"); ?>
 
         <?php
+        
         $sql = "SELECT * FROM commandes";
         $RSCommandes = $pdo->query($sql);
         if(!$RSCommandes){
             die("Echec de la requête :".$RSCommandes->errorInfo());
         }
-        echo "Bonjour";       
-
-        ?>
+              
         
-        <p> Salut </p>
 
+        foreach($RSCommandes as $rowRSCommandes){
+            echo $rowRSCommandes["Commande_Date"];
+        }
+        ?>
+
+
+
+
+        
+        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
