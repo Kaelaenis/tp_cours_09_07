06aa1ed4d4aa50f4d8f3d5fe546eed7f6d1ba372
Valentin Coulon <Valentin.coulon5@outlook.fr>
2021-07-09 12:05:29 +0200

    test

--- commandes.php
+++ commandes.php
@@ -1,12 +1,16 @@
 <?php include("header.php") ?>
+<?php include("class.php") ?>
     <?php 
-            $sql = "SELECT * FROM cmd_pdt";
+            $sql = "SELECT * FROM cmd_pdt WHERE Cmd_Pdt_ID=".$_GET['id'];
             $commandes = $pdo->query($sql);
             if (!$commandes){
                 die("Echec de la requête !".$commandes->errorInfo());
             }
             
     ?>
+    <div class="container">
+        <h2>Commande N°<?php echo $commandes['Cmd_Pdt_ID'] ?></h2>
+    </div>
     <table class="table">
         <thead>
             <tr>
@@ -18,10 +22,12 @@
         </thead>
         <tbody>
             <?php 
-            foreach($commandes as $donnees){ 
+            foreach($commandes as $donnees){
+                $sql = "SELECT Cmd_Pdt_Commande_ID FROM cmd_pdt ORDER BY DESC";
+                $ordre = $pdo->query($sql);
             ?>
             <tr>
-                <th scope="row"><?php echo $donnees['Cmd_Pdt_Produit_ID'] ?></th>
+                <th scope="row"><?php echo $donnees['Cmd_Pdt_Commande_ID']; ?></th>
                 <td><?php echo $donnees['Commande_Date']; ?></td>
                 <td><?php echo $donnees['Commande_Client_ID']; ?></td>
                 <td><?php echo $RSNom['Produit_Nom']; ?></td>
