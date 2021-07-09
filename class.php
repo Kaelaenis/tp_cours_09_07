b0e22931049020e72cc045a49046ae65a457dbee
Valentin Coulon <Valentin.coulon5@outlook.fr>
2021-07-09 10:14:32 +0200

    class_finie

--- class.php
+++ class.php
@@ -1,4 +1,15 @@
 <?php 
 
     define("SERVER", "localhost"); //Définition de l'adresse du serveur
-    define("DBNAME", "aden_bdd7"); //Définition de la base de donnée
+    define("DBNAME", "aden_bdd7"); //Définition de la base de données
+    define("USER", "root"); //Défintion du nom d'utilisateur
+    define("PASSWORD", "root"); // Définition du mot de passe user
+
+    try{
+        // Connexion à la base de données
+        $pdo = new PDO("mysql:host=".SERVER?"; dbname=".DBNAME."; charset=utf8", USER, PASSWORD);
+    }catch(PDOException $e){
+        // Gestion des erreurs si erreur
+        print("Erreur ! ".$e->getMessage());
+        die();
+    }
