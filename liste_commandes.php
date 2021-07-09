1ed7f6d0e11085720e8eba56f2ba2efa02f7b97a
Valentin Coulon <Valentin.coulon5@outlook.fr>
2021-07-09 11:14:11 +0200

    modif

--- liste_commandes.php
+++ liste_commandes.php
@@ -10,15 +10,15 @@
         <?php require("class.php"); ?>
 
         <?php
-        
         $sql = "SELECT * FROM commandes";
         $RSCommandes = $pdo->query($sql);
         if(!$RSCommandes){
             die("Echec de la requête :".$RSCommandes->errorInfo());
         }
-        echo "Bonjour";
+        echo "Bonjour"; 
 
-        ?>
+        ?>       
+
         <p>Salut</p>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
